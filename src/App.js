70c456b46198c1816ab5924c28b6524e56918ac8
Dmitriy <dimaarion@gmail.com>
2023-05-18 12:35:33 +0000

    Update App.js and styles.css

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ export default function App() {
     <div className="expres-calculator">
       <div className="entry ">
         <div className="row">
-          <div className="col-sm">
-            <input type="number" className="form-control" />
+          <div className="col enact">
+            <input type="number" className="form-control" min="1" max="10" />
           </div>
-          <div className="col-sm">
+          <div className="col text-center">
             <Equal />
+          </div>
+          <div className="col p-0 position-relative">
+            <div className="position-absolute result">0</div>
           </div>
         </div>
       </div>
