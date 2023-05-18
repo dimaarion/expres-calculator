7bdd2ef97ad06da781c3fe74388d13f805595731
Dmitriy <dimaarion@gmail.com>
2023-05-18 10:52:13 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./styles.css";
-
+import Equal from "./Equal";
 export default function App() {
-  return <div className="expres-calculator">
-    
-  </div>;
+  return (
+    <div className="expres-calculator">
+      <div className="entry ">
+        <div className="row">
+          <div className="col-sm">
+            <input type="number" className="form-control" />
+          </div>
+          <div className="col-sm">
+            <Equal />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
